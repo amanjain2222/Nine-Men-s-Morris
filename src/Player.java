ad04490710a343b685d4e8c210f47ac84c7b9afa
Tharith Yeak <tyea0002@student.monash.edu>
2023-04-24 17:16:34 +1000

    Draft codes for Player.java, ToDo: Code the getMove and getRemovePosition.

---
+++ src/Player.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Player {
+    private final String name;
+    private final char displayChar;
+    private int piecesRemaining;
+    private int piecesOnBoard;
+
+    public Player(String name, char displayChar, int piecesRemaining) {
+        this.name = name;
+        this.displayChar = displayChar;
+        this.piecesRemaining = piecesRemaining;
+        this.piecesOnBoard = 0;
+    }
+
+    public String getName() {
+        // return the name of the player
+        return name;
+    }
+
+    public char getDisplayChar() {
+        // return the display character of the player
+        return displayChar;
+    }
+
+    public int getPiecesRemaining() {
+        //  return the number of pieces remaining
+        return piecesRemaining;
+    }
+
+    public int getPiecesOnBoard() {
+        // return the number of pieces on the board
+        return piecesOnBoard;
+    }
+
+    public void addPieceOnBoard() {
+        // add a piece on the board
+        piecesOnBoard++;
+    }
+
+    public void removePieceOnBoard() {
+        // remove a piece on the board
+        piecesOnBoard--;
+    }
+
+    public void decreasePiecesRemaining() {
+        // decrease the number of pieces remaining
+        piecesRemaining--;
+    }
+
+    public boolean isAllPiecesOnBoard() {
+        // check if all pieces are on the board
+        return piecesRemaining == 0;
+    }
+
+    public Move getMove() {
+        //ToDo: write code for getting move from player, this is where the game input is taken
+        return null;
+    }
+
+    public int getRemovePosition() {
+        // ToDo: write code for prompts the player to select a position to remove a piece from the board
+        return 0;
+    }
+}
