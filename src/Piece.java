435094211378f4c1717ba64641cb89f862001009
Tharith Yeak <tyea0002@student.monash.edu>
2023-04-24 17:19:40 +1000

    add comments to methods in Piece.java

--- src/Piece.java
+++ src/Piece.java
@@ -13,34 +13,42 @@ public class Piece {
     }
 
     public Player getOwner() {
+        // return the owner of the piece
         return owner;
     }
 
     public char getDisplayChar() {
+        // return the display character of the piece
         return displayChar;
     }
 
     public Position getPosition() {
+        // return the position of the piece
         return position;
     }
 
     public void setPosition(Position position) {
+        // set the position of the piece
         this.position = position;
     }
 
     public boolean isPlacedOnBoard() {
+        // check if the piece is placed on the board
         return placedOnBoard;
     }
 
     public void setPlacedOnBoard() {
+        // set the piece to be placed on the board
         this.placedOnBoard = true;
     }
 
     public boolean isRemovedFromBoard() {
+        // check if the piece is removed from the board
         return removedFromBoard;
     }
 
     public void setRemovedFromBoard() {
+        // set the piece to be removed from the board
         this.removedFromBoard = true;
     }
 }
