b52619af5deaf6e9cb7df51d7b975ef63239168f
Tharith Yeak <tyea0002@student.monash.edu>
2023-04-24 16:41:41 +1000

    Draft code for Piece.java

---
+++ src/Piece.java
@@ -0,0 +1,46 @@
+public class Piece {
+    private final char displayChar;
+    private final Player owner;
+    private Position position;
+    private boolean placedOnBoard;
+    private boolean removedFromBoard;
+
+    public Piece(char displayCharacter, Player owner) {
+        this.displayChar = displayCharacter;
+        this.owner = owner;
+        this.placedOnBoard = false;
+        this.removedFromBoard = false;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public char getDisplayChar() {
+        return displayChar;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public boolean isPlacedOnBoard() {
+        return placedOnBoard;
+    }
+
+    public void setPlacedOnBoard(boolean placedOnBoard) {
+        this.placedOnBoard = placedOnBoard;
+    }
+
+    public boolean isRemovedFromBoard() {
+        return removedFromBoard;
+    }
+
+    public void setRemovedFromBoard(boolean removedFromBoard) {
+        this.removedFromBoard = removedFromBoard;
+    }
+}
