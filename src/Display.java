43b33649cb43e44517a24b30a749233479a371ec
Tharith Yeak <tyea0002@student.monash.edu>
2023-04-26 14:08:59 +1000

    changes to display main codes

--- src/Display.java
+++ src/Display.java
@@ -4,31 +4,29 @@ public class Display {
 
     public Display(Board board) {
         this.board = board;
-        printBoard();
-
     }
 
     private void printBoard() {
         String[][] gameBoard = {
-                {showPosition(0) + "---------------" + showPosition(1) +  "----------------" + showPosition(2)},
+                {"{" + showPosition(0) + "}---------------" + "{" + showPosition(1) +  "}----------------" + "{" + showPosition(2) + "}"},
                 {" |                 |                  | "},
                 {" |                 |                  | "},
-                {" |   " + showPosition(3)+ "----------" + showPosition(4) + "----------_" + showPosition(5) + "   | "},
+                {" |   {" + showPosition(3) + "}----------{" + showPosition(4) + "}-----------{" + showPosition(5) + "}   | "},
                 {" |    |            |             |    | "},
                 {" |    |            |             |    | "},
-                {" |    |   " + showPosition(6) + "-----" + showPosition(7) + "------" + showPosition(8) + "   |    | "},
+                {" |    |   {" + showPosition(6) + "}-----{" + showPosition(7) + "}------{" + showPosition(8) + "}   |    | "},
                 {" |    |    |                |    |    | "},
                 {" |    |    |                |    |    | "},
-                {showPosition(9) + "--" + showPosition(10) + "--" + showPosition(11) + "              " + showPosition(12) + "--" + showPosition(13) + "--" + showPosition(14)},
+                {"{" + showPosition(9) + "}--{" + showPosition(10) + "}--{" + showPosition(11) + "}              {" + showPosition(12) + "}--{" + showPosition(13) + "}--{" + showPosition(14) + "}"},
                 {" |    |    |                |    |    | "},
                 {" |    |    |                |    |    | "},
-                {" |    |   " + showPosition(15) + "-----" + showPosition(16) + "----_-" + showPosition(17) + "   |    |"},
+                {" |    |   {" + showPosition(15) + "}-----{" + showPosition(16) + "}------{" + showPosition(17) + "}   |    |"},
                 {" |    |            |             |    | "},
                 {" |    |            |             |    | "},
-                {" |   " + showPosition(18)+ "----------" + showPosition(19) + "----------_" + showPosition(20) + "   | "},
+                {" |   {" + showPosition(18)+ "}----------{" + showPosition(19) + "}-----------{" + showPosition(20) + "}   | "},
                 {" |                 |                  | "},
                 {" |                 |                  | "},
-                {showPosition(21) + "---------------" + showPosition(22) +  "----------------" + showPosition(23)}};
+                {"{" + showPosition(21) + "}---------------{" + showPosition(22) +  "}----------------{" + showPosition(23) + "}"}};
 
 
         System.out.println();
@@ -41,17 +39,19 @@ public class Display {
     }
 
     private char showPosition (int positionIndex) {
+        // Return the character that represents the piece occupying the position
         if (this.board.getPosition(positionIndex).isEmpty()) {
-            return ' ';
+            return ' '; // Return a space if the position is empty
         }
         else {
             return this.board.getPosition(positionIndex).getPieceOccupying().getDisplayChar();
         }
     }
 
-    public static Display main(String[] args) {
+    public static void main(String[] args) {
         Game newGame = new Game();
         Display consoleDisplay = new Display(newGame.getGameBoard());
-        return consoleDisplay;
+        consoleDisplay.printBoard();
     }
+
 }
