e7eebccea8237ecb065d35908ac354e72c97baf8
Tharith Yeak <tyea0002@student.monash.edu>
2023-04-26 12:43:37 +1000

    update Player to have ArrayList instances to store the list of Piece objects and methods to access and mutate it

--- src/Player.java
+++ src/Player.java
@@ -1,65 +1,82 @@
-import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Player {
     private final String name;
     private final char displayChar;
-    private int piecesRemaining;
-    private int piecesOnBoard;
+    private ArrayList<Piece> piecesRemaining;
+    private int numOfPiecesOnBoard;
 
-    public Player(String name, char displayChar, int piecesRemaining) {
+    public Player(String name, char displayChar) {
         this.name = name;
         this.displayChar = displayChar;
-        this.piecesRemaining = piecesRemaining;
-        this.piecesOnBoard = 0;
+        this.numOfPiecesOnBoard = 0;
+        this.piecesRemaining = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            this.piecesRemaining.add(new Piece(displayChar, this));
+        }
     }
 
     public String getName() {
         // return the name of the player
-        return name;
+        return this.name;
     }
 
     public char getDisplayChar() {
         // return the display character of the player
-        return displayChar;
+        return this.displayChar;
     }
 
-    public int getPiecesRemaining() {
+    public ArrayList<Piece> getPiecesRemaining() {
+        // return the pieces remaining
+        return this.piecesRemaining;
+    }
+
+    public int getNumOfPiecesRemaining() {
         //  return the number of pieces remaining
-        return piecesRemaining;
+        return this.piecesRemaining.size();
     }
 
-    public int getPiecesOnBoard() {
+    public int getNumOfPiecesOnBoard() {
         // return the number of pieces on the board
-        return piecesOnBoard;
+        return this.numOfPiecesOnBoard;
     }
 
-    public void increasePiecesOnBoard() {
+    public void increaseNumOfPiecesOnBoard() {
         // increase the number of pieces on the board
-        piecesOnBoard++;
+        this.numOfPiecesOnBoard++;
     }
 
-    public void decreasePiecesOnBoard() {
+    public void decreaseNumOfPiecesOnBoard() {
         // decrease the number of pieces on the board
-        piecesOnBoard--;
-    }
-
-    public void decreasePiecesRemaining() {
-        // decrease the number of pieces remaining
-        piecesRemaining--;
+        this.numOfPiecesOnBoard--;
     }
 
     public boolean isAllPiecesOnBoard() {
         // check if all pieces are on the board
-        return piecesRemaining == 0;
+        return this.piecesRemaining.size() == 0;
+    }
+
+    public Piece popPiecesRemaining() {
+        // pop a piece from the pieces remaining
+        return this.piecesRemaining.remove(this.piecesRemaining.size() - 1);
+    }
+
+    public void addPiecesRemaining(Piece piece) {
+        // add a piece to the pieces remaining
+        this.piecesRemaining.add(piece);
     }
 
     public Move getMove() {
-        //ToDo: write code for getting move from player, this is where the game input is taken
+        //TODO: write code for getting move from player, this is where the game input is taken
         return null;
     }
 
     public int getRemovePosition() {
-        // ToDo: write code for prompts the player to select a position to remove a piece from the board
+        // TODO: write code for prompts the player to select a position to remove a piece from the board
         return 0;
+    }
+
+    public void undoRemovePieceFromBoard() {
+        //TODO: write code for undoing the removal of a piece from the board
     }
 }
