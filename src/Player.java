987b101b55939cad4ed14f89bed414b7c8e73e53
Tharith Yeak <tyea0002@student.monash.edu>
2023-04-27 17:40:35 +1000

    split move to AdjacentMove

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 public class Player {
     private final String name;
     private final char displayChar;
@@ -66,13 +68,35 @@ public class Player {
         this.piecesRemaining.add(piece);
     }
 
-    public Move getMove() {
+    public Move getAdjacentMove(Board board, Piece piece) {
         //TODO: write code for getting move from player, this is where the game input is taken
+
+        // create new move objects for each type of move
+        Move adjacentMove = new AdjacentMove(this, board, piece);
+
+        // ask user input for start position
+        Scanner startPiece = new Scanner(System.in);
+        System.out.print("Please enter the position of the piece that you want to move: ");
+        String startPieceInput = startPiece.nextLine();
+        // ask user inout for end position
+        Scanner endPiece = new Scanner(System.in);
+        System.out.print("Please enter the position you want to move the piece to: ");
+        String endPieceInput = endPiece.nextLine();
+
+        // execute the move
+        if (adjacentMove.isValidStartPosition(Integer.parseInt(startPieceInput)) && adjacentMove.isValidEndPosition(Integer.parseInt(endPieceInput))){
+            // make the move
+            adjacentMove.execute(Integer.parseInt(startPieceInput), Integer.parseInt(endPieceInput));
+        } else {
+            // fail to make the move and ask user to try again
+            System.out.println("Please try again!");
+            getAdjacentMove(board, piece);
+        }
         return null;
     }
 
     public int getRemovePosition() {
-        // TODO: write code for prompts the player to select a position to remove a piece from the board
+        // TODO: write code for prompts the player to select a position to remove a piece from the board after
         return 0;
     }
 
