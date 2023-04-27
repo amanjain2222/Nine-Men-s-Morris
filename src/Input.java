eabb304ccaaa279b81471040542f38d77a9e1eee
something-gemstone <jadedpsoftware@gmail.com>
2023-04-28 01:16:08 +1000

    Added input class to act as abstraction layer between display input and game input.

---
+++ src/Input.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Input {
+    public ArrayList<Integer> inputValues;
+
+    public Input(ArrayList<Integer> inputValues) {
+        this.inputValues = inputValues;
+    }
+}
