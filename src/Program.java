41e46a9aebc6b8465a6983444a5e093d229daaa3
something-gemstone <jadedpsoftware@gmail.com>
2023-04-28 15:58:45 +1000

    Update game flow to make more logical sense.
    update game first so it can be displayed,
    update display so the user can make an inputs,
    pass inputs to game to be processed next step.

--- src/Program.java
+++ src/Program.java
@@ -9,10 +9,12 @@ public class Program {
         Game game = new Game();
 
         while(true) {
+            // Game Updates By Processing Any Pending Input and Resolving Any Game Constraints
+            // game.updateGame();
             // Query GameState and use it to update the display.
             //GameState currentGameState = game.queryGameState(); 
             displayInterface.updateDisplay(game);
-            // Query InputState and use it to update the game.
+            // Query InputState and queue it to be processed in Game object.
             InputState currentInputState = displayInterface.queryInputState(game);
             //game.queueInputProcess(currentInputState);
             game.updateGame(currentInputState);
