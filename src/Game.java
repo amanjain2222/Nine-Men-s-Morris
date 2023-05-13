763c8b7175377275de1c6f6ad8cc63c0b8e854cf
Tharith Yeak <tyea0002@student.monash.edu>
2023-05-13 17:25:30 +1000

    Update game class to handle errorcode return and add movement console description

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,5 @@
 public class Game {
     private final int PLAYER_STARTING_PIECES = 9;
-
     private final String FIRST_PLAYER_NAME = "Ice";
     private final Character FIRST_PLAYER_CHAR = 'I';
     private final String SECOND_PLAYER_NAME = "Fire";
@@ -14,26 +13,40 @@ public class Game {
     private String currentGamePhase;
     // Keeps Track of whether Previous Game Move was Valid
     private boolean previousMoveInvalid = false;
+    private ErrorCode errorCode;
+    private String moveConsoleDescription = "";
 
     public Game() {
         gameTurn = 0;
         gameBoard = new Board(players[0], players[1]);
         currentGamePhase = "PLACEMENT";
+
     }
 
     public void updateGame(InputState input) {
         // Take Player Turn or Determine if Player Move was Invalid
-        boolean moveStatus = false;
+        ErrorCode moveStatusCode = ErrorCode.UNKNOWN;
+        String moveDescription = "";
+
         if (getCurrentGamePhase().equals("PLACEMENT")) {
             Position targetPosition = gameBoard.getPosition(input.inputValues.get(0));
-            moveStatus = players[gameTurn % players.length].makePlaceMove(targetPosition);
+            moveStatusCode = players[gameTurn % players.length].makePlaceMove(gameBoard,targetPosition);
+            moveDescription = players[gameTurn % players.length].getName() + " Player placed a piece at Position "
+                    + targetPosition.getPositionNumber() + ".";
+
         } else if (getCurrentGamePhase().equals("MOVEMENT")) {
             Position startingPosition = gameBoard.getPosition(input.inputValues.get(0));
             Position targetPosition =  gameBoard.getPosition(input.inputValues.get(1));
-            moveStatus = players[gameTurn % players.length].makeAdjacentMove(startingPosition, targetPosition); 
+            moveStatusCode = players[gameTurn % players.length].makeAdjacentMove(startingPosition, targetPosition);
+            moveDescription = players[gameTurn % players.length].getName() + " Player moved a piece from Position "
+                    + startingPosition.getPositionNumber() + " to Position " + targetPosition.getPositionNumber() + ".";
         }
-        setPreviousMoveInvalid(!moveStatus);
-        if (!moveStatus) return;
+
+        setPreviousMoveInvalid(moveStatusCode != ErrorCode.SUCCESS);
+        setErrorCode(moveStatusCode);
+        setMoveConsoleDescription(moveDescription);
+
+        if (moveStatusCode != ErrorCode.SUCCESS) return;
 
         // Only if Move Was Valid Update Game Globals
         gameTurn++;
@@ -57,6 +70,22 @@ public class Game {
 
     private void setPreviousMoveInvalid(boolean wasInvalid) {
         previousMoveInvalid = wasInvalid;
+    }
+
+    private void setErrorCode(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public String getMoveConsoleDescription() {
+        return moveConsoleDescription;
+    }
+
+    public void setMoveConsoleDescription(String moveConsoleDescription) {
+        this.moveConsoleDescription = moveConsoleDescription;
     }
 
     public Board getGameBoard() {
